a68556b5aa0cdbc4c87961103423c813146b4722
Smaran Harihar <smaran.harihar@gmail.com>
2021-10-10 17:37:16 -0700

    Setting up a Basic gRPC API

---
+++ server/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	proto "Basic_API/proto"
+	"context"
+)
+
+type server struct{}
+
+func main() {
+
+}
+
+func (s *server) Add(ctx context.Context, request *proto.Request) (*proto.Response, error) {
+
+}
